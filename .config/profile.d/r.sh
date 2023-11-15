91598f330ab0ba54417e12dac92a7c1852d9b054
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2023-11-14 22:22:15 -0600

    THings

--- .config/profile.d/r.sh
+++ .config/profile.d/r.sh
@@ -1,2 +1,4 @@
-# MPD stuff
+# Rstudio stuff
 export R_PROFILE_USER="${XDG_CONFIG_HOME}/R/Rprofile"
+export R_ENVIRON_USER="${XDG_CONFIG_HOME}/R/Renviron"
+export R_LIBS_USER="${HOME}/Data/R/%p-library/%v"
